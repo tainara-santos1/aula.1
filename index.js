13c05b63caa430988d2138e4506103a1859536c5
tainara-santos1 <132685015+tainara-santos1@users.noreply.github.com>
2024-02-22 08:57:24 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var pessoa = "832.192.304-34"
+
+console.log(`conteúdo inicial da variavel: ${pessoa} | tipo: ${typeof9(pessoa)}`);
+
+pessoa = 259;
+
+console.log(`mudei o conteúdo: ${pessoa}| tipo: ${typeof(pessoa)}`)
